101302d33264bd1250950e14b6c80638ba995277
FernandoHerbas <feer15.herbas@gmail.com>
2020-12-10 18:06:54 -0300

    Comienzo front

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer app permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Palta
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            S.A
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data () {
+      return {
+        items: [
+          { title: 'Home',to: '/' },
+          { title: 'Living',to: '/living' },
+        ],
+      }
+    },
   }
-}
 </script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/dispositivos/Living.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-container>
+      <v-layout>
+        <v-flex>
+          <v-btn
+            color="accents"
+            elevation="10"
+            x-large
+            icon 
+            class="mx-10"
+          >
+            <v-icon>fa-lightbulb</v-icon>
+          </v-btn>
+
+          <v-btn
+          color="accents"
+          elevation="10"
+          x-large
+          icon 
+          >
+            <v-icon>fa-lightbulb</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex text-xs-center align-center>
+          <v-btn
+            color="accents"
+            elevation="10"
+            x-large
+            icon 
+          >
+            <v-icon>fa-lightbulb</v-icon>
+          </v-btn>
+          
+          <v-btn
+          color="accents"
+          elevation="10"
+          x-large
+          icon 
+          >
+            <v-icon>fa-lightbulb</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
